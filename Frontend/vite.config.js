10656a16b820b60fa2516411924d17307e5b8416
Sudhanshu-NITR <sudhanshu.kadam.99@gmail.com>
2025-01-25 14:33:48 +0530

    update API proxy configuration for local development and add Vercel rewrites for production

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api' : "https://organ-link-backend.vercel.app/"
+      '/api' : 'http://localhost:8800'
     },
   },
   plugins: [react()],
